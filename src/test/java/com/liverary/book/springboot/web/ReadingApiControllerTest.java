f4b7e5bdcb7b0a7d16f82331fcd21e27c13eb294
karice <kl529@naver.com>
2022-01-24 17:25:36 +0900

    302 Error로 인해 템플릿만 만듦

--- src/test/java/com/liverary/book/springboot/web/ReadingApiControllerTest.java
+++ src/test/java/com/liverary/book/springboot/web/ReadingApiControllerTest.java
@@ -1,7 +1,10 @@
 package com.liverary.book.springboot.web;
 
+import com.liverary.book.springboot.domain.book.Book;
 import com.liverary.book.springboot.domain.reading.ReadingRepository;
 import com.liverary.book.springboot.domain.reading.Reading;
+import com.liverary.book.springboot.domain.user.Role;
+import com.liverary.book.springboot.domain.user.User;
 import com.liverary.book.springboot.web.dto.reading.ReadingCalcCurrentPageDto;
 import com.liverary.book.springboot.web.dto.reading.ReadingSaveRequestDto;
 import org.junit.After;
@@ -14,6 +17,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.Date;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,30 +45,63 @@ public class ReadingApiControllerTest {
     }
 
     @Test
-    public void Posts_등록된다() throws Exception {
+    public void StartReading_test() throws Exception { // 책 읽기 시작
         //given
-        String bookReport = "안녕하세요";
-        int score = 3;
+        Book test_book = new Book("테스트 책", "김지원", "우리집", "한국", "재밋음", "북커버", "책내용", 500, new Date(2022-01-13));
+        User test_user = new User("jiwon803@gmail.com", Role.ADMIN);
+
         ReadingSaveRequestDto requestDto = ReadingSaveRequestDto.builder()
-                .currentPage(141)
-                .score(3)
-                .isWrittenBookReport(1)
-                .bookReport("안녕하세요")
+                .book(test_book)
+                .user(test_user)
+                .currentPage(1)
+                .score(0)
+                .isWrittenBookReport(0)
+                .bookReport("")
                 .build();
 
-        String url = "http://localhost:" + port + "/api/v1/reading";
+        String url = "http://localhost:" + port + "/api/v1/reading/startreading";
 
         //when
         ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
 
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
         //then
         List<Reading> all = readingRepository.findAll();
-        assertThat(all.get(0).getScore()).isEqualTo(score);
-        assertThat(all.get(0).getBookReport()).isEqualTo(bookReport);
+        assertThat(all.get(0).getBook()).isEqualTo(test_book);
+        assertThat(all.get(0).getUser()).isEqualTo(test_user);
+        assertThat(all.get(0).getCurrentPage()).isEqualTo(1);
+        assertThat(all.get(0).getScore()).isEqualTo(0);
+        assertThat(all.get(0).getIsWrittenBookReport()).isEqualTo(0);
+        assertThat(all.get(0).getBookReport()).isEqualTo("");
     }
 
+    @Test
+    public void SaveBookReport_test() throws Exception { //BookReport 수정
+    }
+
+    @Test
+    public void GiveScore_test() throws Exception { //점수 수정
+    }
+
+    @Test
+    public void CalcPage_test() throws Exception { //페이지 이동
+    }
+
+    @Test
+    public void delete_test() throws Exception { //읽고 있는 중 책 삭제
+    }
+
+    @Test
+    public void findById_test() throws Exception { //id에 따라 reading 조회
+    }
+
+    @Test
+    public void findAll_test() throws Exception { //option과 id에 따라 user나 book 정보를 조회
+    }
+
+    //--------------
     @Test
     public void Posts_수정된다() throws Exception {
         //given
