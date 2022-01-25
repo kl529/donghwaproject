24a13674bc778c24ce9500fc93f73b421f2434f8
kl529 <kl529@naver.com>
2022-01-25 23:25:46 +0900

    나머지 테스트 완료 + List 테스트 아직 미완성

--- src/test/java/com/liverary/book/springboot/domain/reading/ReadingRepositoryTest.java
+++ src/test/java/com/liverary/book/springboot/domain/reading/ReadingRepositoryTest.java
@@ -3,11 +3,14 @@ package com.liverary.book.springboot.domain.reading;
 import com.liverary.book.springboot.domain.book.Book;
 import com.liverary.book.springboot.domain.user.Role;
 import com.liverary.book.springboot.domain.user.User;
+import com.liverary.book.springboot.web.dto.reading.ReadingSaveRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.sql.Date;
@@ -22,10 +25,10 @@ public class ReadingRepositoryTest {
     @Autowired
     ReadingRepository readingRepository;
 
-    @After
-    public void cleanup() {
-        readingRepository.deleteAll();
-    }
+//    @After
+//    public void cleanup() {
+//        readingRepository.deleteAll();
+//    }
 
     @Test
     public void 게시글저장_불러오기() {
@@ -37,21 +40,62 @@ public class ReadingRepositoryTest {
         readingRepository.save(Reading.builder()
                 .book(test_book)
                 .user(test_user)
-                .currentPage(141)
-                .score(3)
-                .isWrittenBookReport(1)
-                .bookReport("안녕하세요")
+                .currentPage(1)
+                .score(0)
+                .isWrittenBookReport(0)
+                .bookReport("")
                 .build());
 
+        Book test_book2 = new Book("테스트 책2", "김지원2", "우리집2", "한국2", "재밋음2", "북커버2", "책내용2", 600, new Date(2022-01-13));
+        User test_user2 = new User("jiwon803@gmail.com22", Role.ADMIN);
+
+        readingRepository.save(Reading.builder()
+                .book(test_book2)
+                .user(test_user2)
+                .currentPage(1)
+                .score(0)
+                .isWrittenBookReport(0)
+                .bookReport("")
+                .build());
+
+        Book test_book = new Book("테스트 책", "김지원", "우리집", "한국", "재밋음", "북커버", "책내용", 500, new Date(2022-01-13));
+        User test_user = new User("jiwon803@gmail.com", Role.ADMIN);
+
+        ReadingSaveRequestDto requestDto = ReadingSaveRequestDto.builder()
+                .book(test_book)
+                .user(test_user)
+                .currentPage(1)
+                .score(0)
+                .isWrittenBookReport(0)
+                .bookReport("")
+                .build();
+
+        String url = "http://localhost:" + 8080 + "/api/v1/reading/startreading";
+
         //when
-        List<Reading> readingList = readingRepository.findAll();
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        //then
+        List<Reading> all = readingRepository.findAll();
+        assertThat(all.get(0).getBook().getTitle()).isEqualTo(test_book.getTitle());
+        assertThat(all.get(0).getUser().getEmail()).isEqualTo(test_user.getEmail());
+        assertThat(all.get(0).getCurrentPage()).isEqualTo(1);
+        assertThat(all.get(0).getScore()).isEqualTo(0);
+        assertThat(all.get(0).getIsWrittenBookReport()).isEqualTo(0);
+        assertThat(all.get(0).getBookReport()).isEqualTo("");
+
+        //when
+        List<Reading> readingList = readingRepository.findAllDescbyBook(1L);
 
         //then
         Reading reading = readingList.get(0);
 
-        assertThat(reading.getCurrentPage()).isEqualTo(141);
-        assertThat(reading.getScore()).isEqualTo(3);
-        assertThat(reading.getIsWrittenBookReport()).isEqualTo(1);
-        assertThat(reading.getBookReport()).isEqualTo("안녕하세요");
+        assertThat(reading.getCurrentPage()).isEqualTo(1);
+        assertThat(reading.getScore()).isEqualTo(0);
+        assertThat(reading.getIsWrittenBookReport()).isEqualTo(0);
+        assertThat(reading.getBookReport()).isEqualTo("");
     }
 }
--- src/test/java/com/liverary/book/springboot/web/ReadingApiControllerTest.java
+++ src/test/java/com/liverary/book/springboot/web/ReadingApiControllerTest.java
@@ -6,6 +6,8 @@ import com.liverary.book.springboot.domain.reading.Reading;
 import com.liverary.book.springboot.domain.user.Role;
 import com.liverary.book.springboot.domain.user.User;
 import com.liverary.book.springboot.web.dto.reading.ReadingCalcCurrentPageDto;
+import com.liverary.book.springboot.web.dto.reading.ReadingGiveScoreDto;
+import com.liverary.book.springboot.web.dto.reading.ReadingSaveBookReportDto;
 import com.liverary.book.springboot.web.dto.reading.ReadingSaveRequestDto;
 import org.junit.After;
 import org.junit.Test;
@@ -18,7 +20,9 @@ import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.sql.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -79,57 +83,102 @@ public class ReadingApiControllerTest {
 
     @Test
     public void SaveBookReport_test() throws Exception { //BookReport 수정
+        //given
+        Book test_book = new Book("테스트 책", "김지원", "우리집", "한국", "재밋음", "북커버", "책내용", 500, new Date(2022-01-13));
+        User test_user = new User("jiwon803@gmail.com", Role.ADMIN);
+
+        Reading savedReadings = readingRepository.save(Reading.builder()
+                .book(test_book)
+                .user(test_user)
+                .currentPage(1)
+                .score(0)
+                .isWrittenBookReport(0)
+                .bookReport("")
+                .build());
+
+        Long update_reading_key = savedReadings.getReadingKey();
+        String expectedContent = "변경완료";
+
+        ReadingSaveBookReportDto requestDto = ReadingSaveBookReportDto.builder()
+                .bookReport(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/reading/savebookreport/" + update_reading_key;
+
+        //when
+        HttpEntity<ReadingSaveBookReportDto> requestEntity = new HttpEntity<>(requestDto);
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        //then
+        List<Reading> all = readingRepository.findAll();
+        assertThat(all.get(0).getCurrentPage()).isEqualTo(1);
+        assertThat(all.get(0).getBookReport()).isEqualTo(expectedContent);
+
     }
 
     @Test
     public void GiveScore_test() throws Exception { //점수 수정
+        //given
+        Book test_book = new Book("테스트 책", "김지원", "우리집", "한국", "재밋음", "북커버", "책내용", 500, new Date(2022-01-13));
+        User test_user = new User("jiwon803@gmail.com", Role.ADMIN);
+
+        Reading savedReadings = readingRepository.save(Reading.builder()
+                .book(test_book)
+                .user(test_user)
+                .currentPage(1)
+                .score(0)
+                .isWrittenBookReport(0)
+                .bookReport("")
+                .build());
+
+        Long update_reading_key = savedReadings.getReadingKey();
+        int expectedScore = 4;
+
+        ReadingGiveScoreDto requestDto = ReadingGiveScoreDto.builder()
+                .score(expectedScore)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/reading/givescore/" + update_reading_key;
+
+        //when
+        HttpEntity<ReadingGiveScoreDto> requestEntity = new HttpEntity<>(requestDto);
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        //then
+        List<Reading> all = readingRepository.findAll();
+        assertThat(all.get(0).getBook().getTitle()).isEqualTo("테스트 책");
+        assertThat(all.get(0).getScore()).isEqualTo(4);
     }
 
     @Test
     public void CalcPage_test() throws Exception { //페이지 이동
-    }
-
-    @Test
-    public void delete_test() throws Exception { //읽고 있는 중 책 삭제
-    }
-
-    @Test
-    public void findById_test() throws Exception { //id에 따라 reading 조회
-    }
-
-    @Test
-    public void findAll_test() throws Exception { //option과 id에 따라 user나 book 정보를 조회
-    }
-
-    //--------------
-    @Test
-    public void Posts_수정된다() throws Exception {
         //given
-        Reading savedPosts = readingRepository.save(Reading.builder()
-                .currentPage(141)
-                .score(3)
-                .isWrittenBookReport(1)
-                .bookReport("안녕하세요")
+        Book test_book = new Book("테스트 책", "김지원", "우리집", "한국", "재밋음", "북커버", "책내용", 500, new Date(2022-01-13));
+        User test_user = new User("jiwon803@gmail.com", Role.ADMIN);
+
+        Reading savedReadings = readingRepository.save(Reading.builder()
+                .book(test_book)
+                .user(test_user)
+                .currentPage(1)
+                .score(0)
+                .isWrittenBookReport(0)
+                .bookReport("")
                 .build());
 
-        Long updateId = savedPosts.getReadingKey();
-        String expectedContent = "변경완료";
-        int expectedscore = 4;
+        Long update_reading_key = savedReadings.getReadingKey();
+        int expectedPage = 2;
 
         ReadingCalcCurrentPageDto requestDto = ReadingCalcCurrentPageDto.builder()
                 .option(1)
                 .build();
 
-//        readingUpdateRequestDto requestDto = readingUpdateRequestDto.builder()
-//                .currentPage(141)
-//                .score(expectedscore)
-//                .isWrittenBookReport(1)
-//                .bookReport(expectedContent)
-//                .userKey(100L)
-//                .bookKey(10L)
-//                .build();
-
-        String url = "http://localhost:" + port + "/api/v1/reading/calcpage/" + updateId;
+        String url = "http://localhost:" + port + "/api/v1/reading/calcpage/" + update_reading_key;
 
         //when
         HttpEntity<ReadingCalcCurrentPageDto> requestEntity = new HttpEntity<>(requestDto);
@@ -140,7 +189,84 @@ public class ReadingApiControllerTest {
 
         //then
         List<Reading> all = readingRepository.findAll();
-        assertThat(all.get(0).getCurrentPage()).isEqualTo(142);
-//        assertThat(all.get(0).getScore()).isEqualTo(expectedscore);
+        assertThat(all.get(0).getBook().getTitle()).isEqualTo("테스트 책");
+        assertThat(all.get(0).getCurrentPage()).isEqualTo(expectedPage);
     }
+
+    @Test
+    public void delete_test() throws Exception { //읽고 있는 중 책 삭제
+        //given
+        Book test_book = new Book("테스트 책", "김지원", "우리집", "한국", "재밋음", "북커버", "책내용", 500, new Date(2022-01-13));
+        User test_user = new User("jiwon803@gmail.com", Role.ADMIN);
+
+        Reading savedReadings = readingRepository.save(Reading.builder()
+                .book(test_book)
+                .user(test_user)
+                .currentPage(1)
+                .score(0)
+                .isWrittenBookReport(0)
+                .bookReport("")
+                .build());
+
+        Long update_reading_key = savedReadings.getReadingKey();
+        String url = "http://localhost:" + port + "/api/v1/reading/" + update_reading_key;
+
+        //when
+        restTemplate.delete(url);
+
+        //경민 테스트용 -> param을 따로 떼어줌!!!
+//        url = "http://localhost:"+port+"/api/v1/books/{id}";
+//
+//        //when
+//        Map< String, String > params = new HashMap<String, String >();
+//        params.put("id", "0");
+//        restTemplate.delete(url, params);
+
+        //then
+        List<Reading> all = readingRepository.findAll();
+        assertThat(all.size()).isEqualTo(0);
+
+    }
+
+    @Test
+    public void findById_test() throws Exception { //id에 따라 reading 조회
+        //given
+        Book test_book = new Book("테스트 책", "김지원", "우리집", "한국", "재밋음", "북커버", "책내용", 500, new Date(2022-01-13));
+        User test_user = new User("jiwon803@gmail.com", Role.ADMIN);
+
+        Reading savedReadings = readingRepository.save(Reading.builder()
+                .book(test_book)
+                .user(test_user)
+                .currentPage(1)
+                .score(0)
+                .isWrittenBookReport(0)
+                .bookReport("")
+                .build());
+
+        Long update_reading_key = savedReadings.getReadingKey();
+        int expectedPage = 2;
+
+        ReadingCalcCurrentPageDto requestDto = ReadingCalcCurrentPageDto.builder()
+                .option(1)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/reading/calcpage/" + update_reading_key;
+
+        //when
+        HttpEntity<ReadingCalcCurrentPageDto> requestEntity = new HttpEntity<>(requestDto);
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        //then
+        List<Reading> all = readingRepository.findAll();
+        assertThat(all.get(0).getBook().getTitle()).isEqualTo("테스트 책");
+        assertThat(all.get(0).getCurrentPage()).isEqualTo(expectedPage);
+    }
+
+    @Test
+    public void findAll_test() throws Exception { //option과 id에 따라 user나 book 정보를 조회
+    }
+
 }
