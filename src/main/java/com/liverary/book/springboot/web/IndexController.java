fa7847d9627d3c094ab2179ac104923af85a053f
yukyeongmin <fibikong@naver.com>
2022-01-25 23:09:10 +0900

    페이지 연결용 indexcontroller추가

--- src/main/java/com/liverary/book/springboot/web/IndexController.java
+++ src/main/java/com/liverary/book/springboot/web/IndexController.java
@@ -1,11 +1,45 @@
 package com.liverary.book.springboot.web;
 
+import com.liverary.book.springboot.config.auth.dto.SessionUser;
 import com.liverary.book.springboot.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final BookService bookService;
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String welcome(Model model){
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getEmail());
+        }
+        return "welcome";
+    }
+
+    @GetMapping("/homepage")
+    public String homepage(Model model){
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getEmail());
+        }
+        return "home";
+    }
+
+    @GetMapping("/book/save")
+    public String bookSave(){
+        return "book-save";
+    }
+
+    @GetMapping("/book/delete")
+    public String bookDelete(){
+        return "book-delete";
+    }
 }
