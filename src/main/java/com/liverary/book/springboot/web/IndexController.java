ce5ebbaf629329a398322b04b8fd3a3700dcc6c3
yess98 <52556037+yess98@users.noreply.github.com>
2022-02-05 15:07:41 +0900

    0205 합침

--- src/main/java/com/liverary/book/springboot/web/IndexController.java
+++ src/main/java/com/liverary/book/springboot/web/IndexController.java
@@ -3,42 +3,30 @@ package com.liverary.book.springboot.web;
 import com.liverary.book.springboot.config.auth.dto.SessionUser;
 import com.liverary.book.springboot.domain.book.Book;
 import com.liverary.book.springboot.service.BookService;
-<<<<<<< HEAD
+
 import com.liverary.book.springboot.web.dto.book.BookIntroDto;
 import com.liverary.book.springboot.web.dto.book.BookResponseDto;
 import com.liverary.book.springboot.web.dto.book.BookUpdateRequestDto;
-=======
-import com.liverary.book.springboot.web.dto.book.BookResponseDto;
->>>>>>> origin/ykm
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-=======
+
+
 
 import javax.servlet.http.HttpSession;
->>>>>>> origin/ykm
+
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final BookService bookService;
-<<<<<<< HEAD
 
-    @GetMapping("/")
-    public String index(Model model){
-        model.addAttribute("books",bookService.findAllDesc());
-        return "index";
-    }
-    @GetMapping("/books/save")
-        public String booksSave(){
-            return "book-save";
-        }
+
+
 
     @GetMapping("/books/update/{id}")
     public String bookUpdate(@PathVariable Long id, Model model ){
@@ -51,13 +39,7 @@ public class IndexController {
         model.addAttribute("books", bookService.findBySearch(search));
         return "search";
     }
-    @GetMapping("/api/v1/books/info/{id}")
-    public String bookDetail(@PathVariable Long id, Model model ){
-        BookResponseDto bookResponseDto = bookService.findById(id);
-        model.addAttribute("book", bookResponseDto);
-        return "book-detail";
-    }
-=======
+
     private final HttpSession httpSession;
 
     @GetMapping("/")
@@ -103,6 +85,6 @@ public class IndexController {
         return "myinfo";
     }
 
->>>>>>> origin/ykm
+
 }
 
